a81ea19efca257530d0d9a64c899dc69c26b9c6c
whitewalker121 <whitewalker121@gmail.com>
2026-03-18 17:20:09 +0530

    Implement scheduled URL pinging in SalonApiApplication
    Added a scheduled task to ping a URL every 4 minutes.

--- src/main/java/com/invoy/salon/SalonApiApplication.java
+++ src/main/java/com/invoy/salon/SalonApiApplication.java
@@ -4,12 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
 public class SalonApiApplication {
+    private final RestTemplate restTemplate = new RestTemplate();
     public static void main(String[] args) {
         SpringApplication.run(SalonApiApplication.class, args);
+    }
+    
+    @Scheduled(fixedRate = 240000) // 4 minutes
+    public void pingUrl() {
+        try {
+            String url = "https://growthhubbe.onrender.com/swagger-ui/index.html";
+            restTemplate.getForObject(url, String.class);
+            System.out.println("Ping success");
+        } catch (Exception e) {
+            System.out.println("Ping failed: " + e.getMessage());
+        }
     }
 }
